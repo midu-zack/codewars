66c62322c154b68c6f84d8b7774b2300b6ccb5dc
midu-zack <midlajmidu0002@gmail.com>
2024-01-12 09:49:31 +0530

    daytwelve

---
+++ 2024/daytwelve.js
@@ -0,0 +1,35 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+      
+    }
+  }
+
+
+
+
+//   Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+function friend(friends){
+ 
+    const result = friends.filter((value)=> value.length === 4 )
+    
+    return result;
+    
+  }
