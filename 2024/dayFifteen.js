d417ca9e8a61b316be6207e8fd6c3686319c0efa
midu-zack <midlajmidu0002@gmail.com>
2024-01-16 09:31:16 +0530

    daysixteen

---
+++ 2024/dayFifteen.js
@@ -0,0 +1,24 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+    return  b ? 'true':'false';
+  }
+
+
+
+//   Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+ 
+function  stringToArray(string) {
+    return string.split(' ');
+}
+
+ 
---
+++ 2024/daysixteen.js
@@ -0,0 +1,36 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+
+
+function checkForFactor (base, factor) {
+  
+    return base % factor === 0 ;
+     
+    
+  }
+
+
+
+
+//   Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+function paperwork(n, m) {
+    return n > 0 && m > 0 ? n * m : 0
+  }
