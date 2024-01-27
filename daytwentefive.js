58bea2e6a07985759a9f7ce9e11dd84a278fa640
midu-zack <midlajmidu0002@gmail.com>
2024-01-27 18:11:39 +0530

    day27

---
+++ daytwentefive.js
@@ -0,0 +1,31 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+
+
+function DNAtoRNA(dna) {
+    return dna.split("T").join("U");
+  }
+
+
+
+
+
+  
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
+function removeChar(str){
+  //You got this!
+   
+   return str.substring(1,str.length-1 )
+ 
+ };
+ 
+ 
---
+++ daytwenteseven.js
@@ -0,0 +1,10 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+
+
+function opposite(number) {
+    //your code here
+    
+   return number *- 1
+    
+  }
