6aeefa76077796a7ca526ea135f54cc39573eccf
midu-zack <midlajmidu0002@gmail.com>
2024-02-10 13:59:39 +0530

    done

---
+++ 2024/feb10.js
@@ -0,0 +1,24 @@
+function minMax(arr){
+     
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+
+    return [min,max]
+  }
+
+console.log(minMax([1,2,3,4,5,3]));
+
+
+
+
+
+function findDifference(a, b) {
+  
+    let result = a.reduce((acc,val)=> acc*val, 1) 
+    let result1 =  b.reduce((acc,val)=> acc*val, 1)
+    
+   return Math.abs(result-result1)
+    
+  }
+
+ console.log( findDifference([2,333,4],[2,8,9]));
